79147a6f6d023f9101c52fea51e823bb555e12e3
Pixel-Synth <vyvwebprogramming@gmail.com>
2025-02-08 18:38:46 +0530

    Add files via upload

---
+++ js/pass-toggle.js
@@ -0,0 +1,17 @@
+const eyes = document.querySelectorAll('.eye');
+const passwords = document.querySelectorAll('.password');
+console.log(eyes);
+eyes.forEach((eye, index) => {
+  eye.addEventListener('click', () => {
+    const password = passwords[index];
+    if (password.type === "password") {
+      password.type = "text";
+      eye.classList.remove('bi-eye-slash');
+      eye.classList.add('bi-eye');
+    } else {
+      password.type = "password";
+      eye.classList.remove('bi-eye');
+      eye.classList.add('bi-eye-slash');
+    }
+  });
+});
