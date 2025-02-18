ced25531c96c9352cfcac4dd27ef91d26fe2e4fb
Pixel-Synth <vyvwebprogramming@gmail.com>
2025-02-18 18:59:14 +0530

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,24 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-    const categories = document.querySelectorAll(".dropdown div");
-    let selectedCategory = "";
-
-    categories.forEach(category => {
-        category.addEventListener("click", function () {
-            selectedCategory = this.innerText;
-            subject = this.parentElement.parentElement.innerHTML;
-            document.getElementById("fill").innerHTML = ("You selected: " + subject + " - " + selectedCategory);
-        });
-    });
-
-    document.getElementById("start-button").addEventListener("click", function (event) {
-        if (selectedCategory === "") {
-            event.preventDefault();
-            document.getElementById("fill").innerHTML = ("Please select a topic before starting the quiz!");
-        } else {
-            alert("Starting quiz on: " + selectedCategory);
-        }
-    });
-});*/
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
     const title = document.querySelector("h1");
